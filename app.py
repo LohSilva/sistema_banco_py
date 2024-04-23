756ce76d2d6b22e7c5adba84f0c835d79770a7ff
LohSilva <lo-ruama_sp@hotmail.com>
2024-04-23 14:24:59 -0300

    Update funções

--- app.py
+++ app.py
@@ -1,53 +1,21 @@
 # Sistema do Banco Py
-saldo = 0.00
-extrato = " "
-numero_saque_diario = 0.00
-limite_maximo_por_saque = 500.00
-LIMITE_SAQUE_DIARIO = 3
-clientes = []
-
-def exibir_extrato(saldo, /, *, extrato):
-    movimentacoes_realizadas = saldo != 0.00 or extrato.strip() != ""
-    print("\n================ EXTRATO BANCÁRIO ================")
-    if movimentacoes_realizadas:
-        print(extrato.strip())
-    else:
-        print("Não foram realizadas movimentações.")
-    print(f"\nSaldo: R$ {saldo:.2f}")
-    print("==================================================")
-
-def exibir_lista_clientes():
-    print("Lista de clientes atualizada:\n")
-    for cliente in clientes:
-        print("Nome:", cliente['nome'])
-        print("Data de Nascimento:", cliente['data_nascimento'])
-        print("CPF:", cliente['CPF'])
-        endereco = cliente['endereco']
-        print("Endereço:")
-        print("  Logradouro:", endereco['logradouro'])
-        print("  Número:", endereco['numero'])
-        print("  Complemento:", endereco['complemento'])
-        print("  Cidade:", endereco['cidade'])
-        print("  Estado:", endereco['estado'])
-        print("  CEP:", endereco['CEP'])
-
 def imprimir_mensagem_erro(mensagem):
     print(f"Operação falhou! {mensagem}")
-
-def depositar(valor, /):
-    #valor = float(input("Digite o valor do depósito: "))
-    if valor > 0.00:
-        global saldo, extrato
+    
+def depositar(valor, saldo, /, extrato):
+    if valor <= 0.00:
+        imprimir_mensagem_erro("O valor do depósito deve ser positivo.")
+        return saldo, extrato
+    else:
         saldo += valor
         extrato += f"Depósito: R$ {valor:.2f}\n"
         return saldo, extrato
-    else:
-        imprimir_mensagem_erro("O valor informado não é válido!")
 
-def sacar(*, valor):
-    #valor = float(input("Digite o valor do saque: "))
-    if valor > 0.00:
-        global saldo, extrato, numero_saque_diario
+def sacar(*, valor, saldo, extrato, numero_saque_diario, limite_maximo_por_saque):
+    if valor <= 0.00:
+        imprimir_mensagem_erro("O valor do saque deve ser positivo.")
+        return saldo, extrato
+    
         excedeu_saldo = valor > saldo
         excedeu_limite = valor > limite_maximo_por_saque
         excedeu_quantidade_saques = numero_saque_diario >= LIMITE_SAQUE_DIARIO
@@ -61,52 +29,21 @@ def sacar(*, valor):
         else:
             saldo -= valor
             extrato += f"Saque: R$ {valor:.2f}\n"
-            numero_saque_diario += 1           
+            numero_saque_diario += 1
+            
+    return saldo, extrato        
+
+def exibir_extrato(saldo, /, *, extrato):
+    movimentacoes_realizadas = saldo != 0.00 or extrato.strip() != ""
+    print("\n================ EXTRATO BANCÁRIO ================")
+    if movimentacoes_realizadas:
+        print(extrato.strip())
     else:
-        imprimir_mensagem_erro("O valor informado é inválido.")
+        print("Não foram realizadas movimentações.")
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print("==================================================")
 
-    return saldo, extrato     
-
-def transferir():
-    valor = float(input("Digite o valor a ser transferido: "))
-    conta_destino = input("Digite o número da conta de destino: ")
-
-    if valor > 0.00:
-        global saldo, extrato
-        if valor <= saldo:
-            saldo -= valor
-            extrato += f"Transferência para conta {conta_destino}: R$ {valor:.2f}\n"
-        else:
-            imprimir_mensagem_erro("Saldo insuficiente.")
-    else:
-        imprimir_mensagem_erro("O valor informado é inválido.")
-
-def valida_cpf(CPF):  
-    for cliente in clientes:
-        if cliente["CPF"] == CPF:
-            print("CPF já cadastrado!")
-            return True   
-    return False
-
-def cadastrar_conta_corrente(cliente):
-    numero_agencia = "0001"
-    contador_numero_conta = 0
-    contador_numero_conta += 1
-
-    numero_conta_corrente = contador_numero_conta
-    numero_conta_corrente = input("Digite o numero da conta corrente: ")
-
-    conta_corrente = {
-        "agencia": numero_agencia,
-        "conta corrente": numero_conta_corrente,
-        "cliente": cliente,
-        "saldo":0.00,
-        "extrato": ""
-    }
-    return conta_corrente
-    print("\nConta corrente cadastrada com sucesso!")
-
-def cadastrar_cliente(): 
+def cadastrar_cliente(clientes):
     nome = input("Informe seu nome: ")
     data_nascimento = input("Digite sua data de nascimento (DD/MM/AAAA): ")
     CPF = input("Informe seu CPF: ")
@@ -117,7 +54,6 @@ def cadastrar_cliente():
     endereco_estado = input("Qual o estado: ")
     endereco_CEP = input("Digite o CEP do endereço: ")
 
-  
     endereco = {
         "logradouro": endereco_logradouro,
         "numero": endereco_numero,
@@ -133,63 +69,117 @@ def cadastrar_cliente():
         "CPF": CPF,
         "endereco": endereco
     }
-    conta_corrente = cadastrar_conta_corrente(cliente)
-
+    clientes.append(cliente)
     print("\nCliente cadastrado com sucesso!")
-    print("\nNumero da agencia: ", conta_corrente["agencia"])
-    print("\nNumero da conta: ", conta_corrente["conta corrente"])
-    print("\nCliente vinculado a conta corrente: ",cliente)
-   
-menu_eletronico = {
+    return clientes
 
-    1: "Depositar",
-    2: "Sacar",
-    3: "Extrato",
-    4: "Transferir",
-    5: "Cadastrar Cliente",
-    0: "Sair"
-}
+def cadastrar_conta_corrente(cliente):
+    numero_agencia = "0001"
+    numero_conta_corrente = input("Digite o numero da conta corrente: ")
+
+    conta_corrente = {
+        "agencia": numero_agencia,
+        "conta_corrente": numero_conta_corrente,
+        "cliente": cliente,
+        "saldo": 0.00,
+        "extrato": ""
+    }
+    print("\nConta corrente cadastrada com sucesso!")
+    return conta_corrente
     
-while True:
+def valida_cpf(CPF, clientes):
+    for cliente in clientes:
+        if cliente["CPF"] == CPF:
+            print("CPF já cadastrado!")
+            return True
+    return False    
+    
+def exibir_lista_clientes(clientes):
+    print("Lista de clientes atualizada:\n")
+    for cliente in clientes:
+        print("Nome:", cliente['nome'])
+        print("Data de Nascimento:", cliente['data_nascimento'])
+        print("CPF:", cliente['CPF'])
+        endereco = cliente['endereco']
+        print("Endereço:")
+        print("  Logradouro:", endereco['logradouro'])
+        print("  Número:", endereco['numero'])
+        print("  Complemento:", endereco['complemento'])
+        print("  Cidade:", endereco['cidade'])
+        print("  Estado:", endereco['estado'])
+        print("  CEP:", endereco['CEP'])
 
-    print("\nOpções disponíveis: \n")
-    for opcao, descricao in menu_eletronico.items():
-        print(f"[{opcao}] {descricao}")
+def transferir(saldo, extrato):
+    valor = float(input("Digite o valor a ser transferido: "))
+    conta_destino = input("Digite o número da conta de destino: ")
 
-    opcao = int(input("\nEscolha a opção desejada: "))    
-
-    if opcao == 1:
-        valor_deposito = float(input("Digite o valor do depósito: "))
-        saldo, extrato = depositar(valor_deposito)
-    elif opcao == 2:
-        valor_saque = float(input("Digite o valor do saque: "))
-        saldo, extrato = sacar(valor = valor_saque)
-    elif opcao == 3:
-        exibir_extrato(saldo, extrato = extrato)        
-    elif opcao == 4:
-        transferir()
-    elif opcao == 5:
-        cadastrar_cliente()
-    elif opcao == 6:
-        if clientes:
-            print("\nClientes cadastrado no sistema:")
-            for i, cliente in enumerate(clientes):
-                print(f"{i+1}. {cliente['nome']} - {cliente['CPF']}")
-            cliente_index = int(input("Digite o número do cliente para vínculo com a nova conta: ")) - 1
-            if 0 <= cliente_index < len(clientes):
-                cliente_selecionado = clientes[cliente_index]
-                conta_corrente = cadastrar_conta_corrente(cliente_selecionado)
-                print("\nConta corrente cadastrada com sucesso!")
-            else:
-                print("\nOpção inválida!")
+    if valor > 0.00:
+        if valor <= saldo:
+            saldo -= valor
+            extrato += f"Transferência para conta {conta_destino}: R$ {valor:.2f}\n"
         else:
-            print("\nNenhum cliente cadastrado. Cadastre um cliente primeiro.")
-    elif opcao == 7:
-        exibir_lista_clientes()    
-    elif opcao == 0:
-        break
+            imprimir_mensagem_erro("Saldo insuficiente.")
     else:
-        print("Opção inválida. Por favor, digite uma opção válida.")
+        imprimir_mensagem_erro("O valor informado é inválido.")
+
+    return saldo, extrato
+
+def main():
+    saldo = 0.00
+    extrato = ""
+    numero_saque_diario = 0.00
+    limite_maximo_por_saque = 500.00
+    LIMITE_SAQUE_DIARIO = 3
+    clientes = []
+
+    while True:
+        print("\nOpções disponíveis: \n")
+        menu_eletronico = {
+            1: "Depositar",
+            2: "Sacar",
+            3: "Extrato",
+            4: "Transferir",
+            5: "Cadastrar Cliente",
+            6: "Cadastrar Conta Corrente",
+            7: "Exibir Lista de Clientes",
+            0: "Sair"
+        }
+        for opcao, descricao in menu_eletronico.items():
+            print(f"[{opcao}] {descricao}")
+
+        opcao = int(input("\nEscolha a opção desejada: "))    
+
+        if opcao == 1:
+            valor_deposito = float(input("Digite o valor do depósito: "))
+            saldo, extrato = depositar(valor_deposito, saldo, extrato)
+        elif opcao == 2:
+            valor_saque = float(input("Digite o valor do saque: "))
+            saldo, extrato = sacar(valor=valor_saque, saldo=saldo, extrato=extrato, numero_saque_diario=numero_saque_diario, limite_maximo_por_saque=limite_maximo_por_saque)
+        elif opcao == 3:
+            exibir_extrato(saldo, extrato=extrato)        
+        elif opcao == 4:
+            saldo, extrato = transferir(saldo, extrato)
+        elif opcao == 5:
+            clientes = cadastrar_cliente(clientes)
+        elif opcao == 6:
+            if clientes:
+                print("\nClientes cadastrados no sistema:")
+                exibir_lista_clientes(clientes)
+                cliente_index = int(input("Digite o número do cliente para vínculo com a nova conta: ")) - 1
+                if 0 <= cliente_index < len(clientes):
+                    cliente_selecionado = clientes[cliente_index]
+                    conta_corrente = cadastrar_conta_corrente(cliente_selecionado)
+                    print("\nConta corrente cadastrada com sucesso!")
+                else:
+                    print("\nOpção inválida!")
+            else:
+                print("\nNenhum cliente cadastrado. Cadastre um cliente primeiro.")
+        elif opcao == 7:
+            exibir_lista_clientes(clientes)    
+        elif opcao == 0:
+            break
+        else:
+            print("Opção inválida. Por favor, digite uma opção válida.")
 
 
 
